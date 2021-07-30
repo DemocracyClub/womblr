cc1514529f8d3f19f66b9d386eb9d05e888a4002
chris48s <chris.shaw480@gmail.com>
2021-07-30 18:52:02 +0100

    Post/Election --> Ballot

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-import csv
 import datetime
 import os
 import random
@@ -30,10 +29,14 @@ SOPN_PUBLISH_DATE = StatementPublishDate()
 
 
 def init():
+    table_info = scraperwiki.sql.execute("PRAGMA table_info(data);")
+    if table_info.get('data'):
+        if 'post_id' in [row[1] for row in table_info['data']]:
+            scraperwiki.sql.execute("DROP TABLE data;")
+
     scraperwiki.sql.execute("""
         CREATE TABLE IF NOT EXISTS data (
-            post_id TEXT,
-            id TEXT,
+            ballot_id TEXT,
             timestamp DATETIME,
             url TEXT,
             poll_open_date TEXT,
@@ -43,9 +46,10 @@ def init():
             sopn_published TEXT,
             CHECK (locked IN (0, 1))
         );""")
+
     scraperwiki.sql.execute("""
         CREATE UNIQUE INDEX IF NOT EXISTS data_timestamp_id_postid_unique
-        ON data (timestamp, id, post_id);""")
+        ON data (timestamp, ballot_id);""")
 
 def post_slack_message(message):
     slack = SlackClient(SLACK_WEBHOOK_URL)
@@ -55,13 +59,6 @@ def call_json_api(url):
     res = requests.get(url)
     res.raise_for_status()
     return res.json()
-
-def call_csv_api(url):
-    res = requests.get(url)
-    res.raise_for_status()
-    decoded_content = res.content.decode('utf-8')
-    cr = csv.DictReader(decoded_content.splitlines(), delimiter=',')
-    return list(cr)
 
 def get_emoji():
     return random.choice([
@@ -123,29 +120,6 @@ def get_slack_message(elections):
 
     return slack_message
 
-def get_posts(candidates):
-    posts = {}
-    for candidate in candidates:
-        if candidate['post_id'] not in posts:
-            posts[candidate['post_id']] = {
-                'known_candidates': 1,
-                'locked': False
-            }
-            ynr_url = 'https://candidates.democracyclub.org.uk/api/v0.9/posts/' + candidate['post_id']
-            print(ynr_url)
-            post = call_json_api(ynr_url)
-            posts[candidate['post_id']]['label'] = post['label']
-            for election in post['elections']:
-                if election['id'] == candidate['election']:
-                    posts[candidate['post_id']]['locked'] = election['candidates_locked']
-        else:
-            posts[candidate['post_id']]['known_candidates'] += 1
-    return posts
-
-
-def requires_country(election_type):
-    return election_type in ['local', 'parl']
-
 
 def get_sopn_date(result):
     election_id = result['election_id']
@@ -165,71 +139,72 @@ def get_sopn_date(result):
         return None
 
 
-def get_elections():
-    elections = []
+def get_ballots():
+    ballots = []
     # get a list of upcoming elections
     ee_url = "https://elections.democracyclub.org.uk/api/elections.json?future=1&limit=100"
     while ee_url:
         print(ee_url)
         ee_data = call_json_api(ee_url)
 
-        if ee_data['results']:
-            for result in ee_data['results']:
-                election_datetime = datetime.datetime.strptime(result['poll_open_date'], '%Y-%m-%d')
-                threshold_date = NOW + ELECTIONS_IN_SCOPE
-                if result['group_type'] and result['group_type'] != 'election' and election_datetime < threshold_date:
-                    # get details of any candidates we hold for this election
-                    election_id = result['election_id']
-                    ynr_url = "https://candidates.democracyclub.org.uk/media/candidates-%s.csv" % (election_id)
-                    print(ynr_url)
+        if not ee_data['results']:
+            continue
 
-                    sopn_date = get_sopn_date(result)
+        ee_ballots = [
+            election for election in ee_data['results']
+            if election ['identifier_type'] == 'ballot'
+        ]
+        for ee_ballot in ee_ballots:
+            election_datetime = datetime.datetime.strptime(ee_ballot['poll_open_date'], '%Y-%m-%d')
+            threshold_date = NOW + ELECTIONS_IN_SCOPE
+            if election_datetime >= threshold_date:
+                continue
 
-                    try:
-                        ynr_data = call_csv_api(ynr_url)
-                        total_candidates = len(ynr_data)
-                        posts = get_posts(ynr_data)
-                    except requests.exceptions.HTTPError:
-                        total_candidates = 0
+            ballot_id = ee_ballot['election_id']
+            sopn_date = get_sopn_date(ee_ballot)
 
-                    if total_candidates == 0 or not posts:
-                        elections.append({
-                            'timestamp': NOW,
-                            'id': election_id,
-                            'name': result['election_title'],
-                            'known_candidates': total_candidates,
-                            'poll_open_date': result['poll_open_date'],
-                            'url': "https://candidates.democracyclub.org.uk/election/%s/constituencies" % (election_id),
-                            'post_id': None,
-                            'locked': False,
-                            'sopn_published': str(sopn_date) if sopn_date is not None else None
-                        })
-                    else:
-                        for post in posts:
-                            elections.append({
-                                'timestamp': NOW,
-                                'id': election_id,
-                                'name': "%s - %s" % (result['election_title'], posts[post]['label']),
-                                'known_candidates': posts[post]['known_candidates'],
-                                'poll_open_date': result['poll_open_date'],
-                                'url': "https://candidates.democracyclub.org.uk/election/%s/post/%s" % (election_id, post),
-                                'post_id': post,
-                                'locked': posts[post]['locked'],
-                                'sopn_published': str(sopn_date) if sopn_date is not None else None
-                            })
-                    time.sleep(2)  # have a little snooze to avoid hammering the api
+            ynr_ballot_url = 'https://candidates.democracyclub.org.uk/api/next/ballots/{}/'.format(ballot_id)
+            print(ynr_ballot_url)
+
+            try:
+                ynr_ballot = call_json_api(ynr_ballot_url)
+            except requests.exceptions.HTTPError:
+                ballots.append({
+                    'timestamp': NOW,
+                    'ballot_id': ballot_id,
+                    'name': ee_ballot['election_title'],
+                    'known_candidates': 0,
+                    'poll_open_date': ee_ballot['poll_open_date'],
+                    'url': "https://candidates.democracyclub.org.uk/elections/{}/".format(ballot_id),
+                    'locked': False,
+                    'sopn_published': str(sopn_date) if sopn_date is not None else None
+                })
+                continue
+
+            ballots.append({
+                'timestamp': NOW,
+                'ballot_id': ballot_id,
+                'name': ee_ballot['election_title'],
+                'known_candidates': len(ynr_ballot['candidacies']),
+                'poll_open_date': ee_ballot['poll_open_date'],
+                'url': "https://candidates.democracyclub.org.uk/elections/{}/".format(ballot_id),
+                'locked': ynr_ballot['candidates_locked'],
+                'sopn_published': str(sopn_date) if sopn_date is not None else None
+            })
+
+            time.sleep(2)  # have a little snooze to avoid hammering the api
 
         time.sleep(1)  # have a little snooze to avoid hammering the api
         ee_url = ee_data['next']
 
-    return elections
+    return ballots
 
 def scrape():
-    elections = get_elections()
+    ballots = get_ballots()
     scraperwiki.sqlite.save(
-        unique_keys=['timestamp', 'id', 'post_id'], data=elections, table_name='data')
+        unique_keys=['timestamp', 'ballot_id'], data=ballots, table_name='data')
     print('=====')
-    slack_message = get_slack_message(elections)
+    slack_message = get_slack_message(ballots)
     print(slack_message)
     if SLACK_WEBHOOK_URL:
         post_slack_message(slack_message)
@@ -246,10 +221,10 @@ if latest[0]['ts'] is None:
 last_run = datetime.datetime.strptime(latest[0]['ts'], '%Y-%m-%d %H:%M:%S.%f')
 if last_run + UPDATE_FREQUENCY > NOW:
     print("Nothing to do today, but here's the results from the last run..")
-    elections = scraperwiki.sql.select(
+    ballots = scraperwiki.sql.select(
         "* FROM 'data' WHERE timestamp=?;", [latest[0]['ts']])
     print('=====')
-    slack_message = get_slack_message(elections)
+    slack_message = get_slack_message(ballots)
     print(slack_message)
 else:
     scrape()
